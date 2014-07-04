39aa089328aab881cb5860ce5af229c5433617d1
Alex Jose <ajose@caltech.edu>
2014-07-04 03:40:30 -0400

    add csv parsing

--- cardmaker.py
+++ cardmaker.py
@@ -2,6 +2,7 @@
 import cairo
 import pango
 import pangocairo
+import csv
 
 """
 TODO
@@ -28,6 +29,17 @@ def p2u(x):
     # units_from_double might do this...
     return int(x*pango.SCALE)
 
+def csv_to_cards(filename):
+    # open csv file and return list of cards for passing to build_pages
+    cards = []
+    with open(filename, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in reader:
+            #print '\n'.join(row)
+            name, pos, hp, pwr, ap, desc = row 
+            cards.append((name, hp, pwr, ap, desc))
+    return cards
+    
 def write(ctx, pctx, layout, x, y, width, text, alignment, center_vert=None):
     # to center vertically, make sure 'y' is the line you want to center on
     ctx.move_to(x,y)
@@ -45,7 +57,7 @@ def write(ctx, pctx, layout, x, y, width, text, alignment, center_vert=None):
     pctx.update_layout(layout)
     pctx.show_layout(layout)
 
-def build_card(title, pwr, hp, ap, desc, save=False):
+def build_card(title, hp,pwr,ap, desc, save=False):
     WIDTH, HEIGHT = 350, 500
     scale = lambda x, y: (x*WIDTH, y*HEIGHT)
     
@@ -127,7 +139,7 @@ def build_card(title, pwr, hp, ap, desc, save=False):
 
     # write stats
     ctx.set_source_rgb(1,1,1)
-    font = pango.FontDescription(fontname + " 25")
+    font = pango.FontDescription(fontname + " 15")
     layout.set_font_description(font)
     w = 0.25*WIDTH
     x,y = scale(.75, .5+1./12)
@@ -139,12 +151,14 @@ def build_card(title, pwr, hp, ap, desc, save=False):
 
     # write description
     ctx.set_source_rgb(0, 0, 0)
-    font = pango.FontDescription(fontname + " 10")
+    font = pango.FontDescription(fontname + " 12")
     layout.set_font_description(font)
     w = .76*WIDTH
-    #x,y = scale(0.0025, .5)
-    x,y = scale(0.0025, .75)
-    write(ctx, pangocairo_ctx, layout, x,y, w, desc, pango.ALIGN_LEFT, True)
+    x,y = scale(0.0025, .5)
+    write(ctx, pangocairo_ctx, layout, x,y, w, desc, pango.ALIGN_LEFT)
+    #x,y = scale(0.0025, .75)
+    #write(ctx, pangocairo_ctx, layout, x,y, w, desc, pango.ALIGN_LEFT, True)
+
 
     # save to PNG if want to
     if save:
@@ -178,7 +192,7 @@ def build_page(cards_w, cards_h, cards, filename="test_page"):
     surf = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
     ctx = cairo.Context(surf)
     ctx.rectangle(0,0, WIDTH, HEIGHT)
-    ctx.set_source_rgb(0, 1, 0)
+    ctx.set_source_rgb(0, 0, 0)
     ctx.fill()
     
     # fill with cards
@@ -197,13 +211,15 @@ def build_page(cards_w, cards_h, cards, filename="test_page"):
     surf.write_to_png(filename+".png")
 
     
-cannon_text = """Cannon can have subchains.
-Cannon can only make ranged attacks with its subchains.
-Subchains used in Cannon's ranged attacks take all damage they deal.
-(Give POW bonus for ranged?)
-(Should allow it to 'summon' ammo? So if you don't want to launch anything can get a cannon ball or something to fire)"""
 if __name__=="__main__":
-    card = ('cannon', 2,15,8, cannon_text)
+    text = "Melee: +1 POW when used near target\n\nEdged"
+    name = 'chainsaw'
+    hp = "8/12/+0"
+    pwr = "4"
+    ap = "-3/0/+0"
+    card = (name, hp,pwr,ap, text, True)
     #build_card(*card)
     #build_page(3,5, [card for _ in range(10)])
-    build_pages(2,2, [card for _ in range(10)])
+    #build_pages(2,2, [card for _ in range(10)])
+    cards = csv_to_cards('cards.csv')
+    build_pages(2, 2, cards)
